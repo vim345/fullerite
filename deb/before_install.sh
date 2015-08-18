fcd6818e4662a0f922f9bd35c8c7871c6a5bc525
Ryan Neal <ryanneal@yelp.com>
2015-08-18 16:18:19 -0700

    fix some small tweaks with the init scripts and add in more logging to before_install

--- deb/before_install.sh
+++ deb/before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-USER="fullerite"
-
-id $USER > /dev/null 2>&1
-if [ $? != 0 ]; then
-  useradd --no-create-home --system --user-group $USER
+if [ "$(id 'fullerite')" ]; then
+  echo "creating user fullerite"
+  useradd --no-create-home --system --user-group "fullerite"
 fi
 
+echo "creating log directory: /var/log/fullerite"
 mkdir -p /var/log/fullerite
+chown fullerite:fullerite /var/log/fullerite
