3352ff0420d03618f719bf1055cbe1fc4dfde9fa
Baris Metin <baris@metin.org>
2015-09-01 23:40:57 -0700

    a more comprehensive test for the signalfx handler

--- src/fullerite/handler/signalfx_test.go
+++ src/fullerite/handler/signalfx_test.go
@@ -2,9 +2,14 @@ package handler_test
 
 import (
 	"fullerite/handler"
+	"fullerite/metric"
 
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,6 +30,7 @@ func TestSignalfxConfigure(t *testing.T) {
 	config["interval"] = "10"
 	config["timeout"] = "10"
 	config["max_buffer_size"] = "100"
+	config["authToken"] = "secret"
 	config["endpoint"] = "signalfx.server"
 
 	s := handler.NewSignalFx()
@@ -46,4 +52,41 @@ func TestSignalfxConfigure(t *testing.T) {
 		config["endpoint"],
 		"should be the set value",
 	)
+}
+
+func TestSignalFxRun(t *testing.T) {
+	assert := assert.New(t)
+
+	wait := make(chan bool)
+	// Mock SignalFx server
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		message := &handler.DataPointUploadMessage{}
+		proto.Unmarshal(body, message)
+		datapoint := message.Datapoints[0]
+
+		assert.Nil(err)
+		assert.Equal(*datapoint.Metric, "Test")
+		assert.Equal(r.Header["Content-Type"], []string{"application/x-protobuf"})
+		assert.Equal(r.Header["X-Sf-Token"], []string{"secret"})
+
+		wait <- true
+	}))
+	defer ts.Close()
+
+	config := make(map[string]interface{})
+	config["interval"] = "1"
+	config["timeout"] = "1"
+	config["max_buffer_size"] = "1"
+	config["authToken"] = "secret"
+	config["endpoint"] = ts.URL
+	s := handler.NewSignalFx()
+	s.Configure(config)
+
+	go s.Run()
+
+	m := metric.New("Test")
+	s.Channel() <- m
+
+	<-wait
 }
