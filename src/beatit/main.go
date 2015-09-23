c83f7fcbcc19f115030b2b28f0c9be16f02d76fd
Baris Metin <baris@metin.org>
2015-09-23 16:06:39 -0700

    bump version to 0.1.4

--- src/beatit/main.go
+++ src/beatit/main.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	name    = "beatit"
-	version = "0.1.3"
+	version = "0.1.4"
 	desc    = "Stress test fullerite handlers"
 )
 
--- src/fullerite/main.go
+++ src/fullerite/main.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	name    = "fullerite"
-	version = "0.1.3"
+	version = "0.1.4"
 	desc    = "Diamond compatible metrics collector"
 )
 
