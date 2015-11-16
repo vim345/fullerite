40b2a8e97ef6675c92ce02b56977e601d45d2528
T SHEASHA <tsheasha@yelp.com>
2015-11-16 14:03:46 -0800

    Adidng more metrics to Scribe collector
    A misisng metric for the Scribe collector was the scribe buffer size,
    this can be done now by defining a `path` entry and an optional exclude_pattern
    in the config file poiting to the scribe_buffer path. This defaults to None and
    will hence not bne reported unless that path is defined.

--- src/diamond/collectors/scribe/scribe.py
+++ src/diamond/collectors/scribe/scribe.py
@@ -20,6 +20,8 @@ class ScribeCollector(diamond.collector.Collector):
     def get_default_config_help(self):
         config_help = super(ScribeCollector, self).get_default_config_help()
         config_help.update({
+            'exclude_pattern': 'Exclude items from scribe buffer that match this pattern',
+            'path': 'Path to scribe buffer',
             'scribe_ctrl_bin': 'Path to scribe_ctrl binary',
             'scribe_port': 'Scribe port',
         })
@@ -28,6 +30,7 @@ class ScribeCollector(diamond.collector.Collector):
     def get_default_config(self):
         config = super(ScribeCollector, self).get_default_config()
         config.update({
+            'exclude_pattern': None,
             'path': 'scribe',
             'scribe_ctrl_bin': self.find_binary('/usr/sbin/scribe_ctrl'),
             'scribe_port': None,
@@ -71,8 +74,32 @@ class ScribeCollector(diamond.collector.Collector):
             metric = self.key_to_metric(key)
             data[metric] = int(val)
 
+        if self.config['path']:
+            cmd = ['du', '-sb', '--apparent-size', self.config['path']]
+            if self.config['exclude_pattern']:
+                cmd.append(
+                    "--exclude={0!s}".format(self.config['exclude_pattern'])
+                )
+            try:
+                p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+                output, errors = p.communicate()
+                if errors:
+                    self.log.error(
+                        "Error running {0!r}, {1!s}".format(cmd, errors)
+                    )
+                else:
+                    data['buffer_size'] = int(output[:output.find('\t')])
+            except OSError:
+                self.log.exception(
+                    "Unable to run {0!r}".format(cmd)
+                )
+
         return data.items()
 
     def collect(self):
         for stat, val in self.get_scribe_stats():
+            self.log.debug(
+                "Publishing: {0} {1}".format(stat, val)
+            )
             self.publish(stat, val)
