d0e30d17fd761c7c9b3fee5d8ef25b04ed4e74cc
Hemant Kumar <gethemant@gmail.com>
2015-11-18 00:36:03 +0000

    Bring dropwizard files inside nervewsgi collector

--- src/fullerite/collector/nerve_uwsgi.go
+++ src/fullerite/collector/nerve_uwsgi.go
@@ -16,6 +16,13 @@ import (
 	l "github.com/Sirupsen/logrus"
 )
 
+const (
+	// MetricTypeCounter String for counter metric type
+	MetricTypeCounter = "COUNTER"
+	// MetricTypeGauge String for Gauge metric type
+	MetricTypeGauge = "GAUGE"
+)
+
 // This is a collector that will parse a config file on collection
 // that is formatted in the standard nerve way. Then it will query
 // all the local endpoints on the given path. The metrics are assumed
@@ -74,6 +81,11 @@ type uwsgiJSONFormat struct {
 	Histograms map[string]map[string]interface{}
 	Meters     map[string]map[string]interface{}
 	Timers     map[string]map[string]interface{}
+}
+
+type NestedMetricMap struct {
+	MetricSegments []string
+	MetricMap      map[string]interface{}
 }
 
 func newNerveUWSGICollector(channel chan metric.Metric, initialInterval int, log *l.Entry) *nerveUWSGICollector {
@@ -327,4 +339,174 @@ func queryEndpoint(endpoint string, timeout int) ([]byte, error) {
 	}
 
 	return txt, nil
+}
+
+func parseDropwizardMetric(raw *[]byte) ([]metric.Metric, error) {
+	var parsed map[string]interface{}
+
+	err := json.Unmarshal(*raw, &parsed)
+
+	if err != nil {
+		return []metric.Metric{}, err
+	}
+
+	metricName := []string{}
+
+	return parseMetricMap(parsed, metricName), nil
+}
+
+func parseMetricMap(
+	jsonMap map[string]interface{},
+	metricName []string) []metric.Metric {
+	results := []metric.Metric{}
+
+	for k, v := range jsonMap {
+		switch t := v.(type) {
+		case map[string]interface{}:
+			metricName = append(metricName, k)
+			tempResults := parseMetricMap(t, metricName)
+			// pop the name, now that it is processed
+			if len(metricName)-1 >= 0 {
+				metricName = metricName[:(len(metricName) - 1)]
+			}
+			results = append(results, tempResults...)
+		default:
+			tempResults := parseFlattenedMetricMap(jsonMap, metricName)
+			if len(tempResults) > 0 {
+				results = append(results, tempResults...)
+				return results
+			}
+			m, ok := extractGaugeValue(k, v, metricName)
+			if ok {
+				results = append(results, m)
+			}
+		}
+	}
+
+	return results
+}
+
+func extractGaugeValue(key string, value interface{}, metricName []string) (metric.Metric, bool) {
+	compositeMetricName := strings.Join(append(metricName, key), ".")
+	return createMetricFromDatam("value", value, compositeMetricName, "GAUGE")
+}
+
+func parseFlattenedMetricMap(jsonMap map[string]interface{}, metricName []string) []metric.Metric {
+	if t, ok := jsonMap["type"]; ok {
+		metricType := t.(string)
+		if metricType == "gauge" {
+			return collectGauge(jsonMap, metricName, "gauge")
+		} else if metricType == "histogram" {
+			return collectHistogram(jsonMap, metricName, "histogram")
+		} else if metricType == "counter" {
+			return collectCounter(jsonMap, metricName, "counter")
+		} else if metricType == "meter" {
+			return collectMeter(jsonMap, metricName)
+		}
+	}
+
+	// if nothing else works try for rate
+	return collectRate(jsonMap, metricName)
+}
+
+func collectGauge(jsonMap map[string]interface{}, metricName []string,
+	metricType string) []metric.Metric {
+
+	compositeMetricName := strings.Join(metricName, ".")
+	return metricFromMap(&jsonMap, compositeMetricName, metricType)
+}
+
+func collectHistogram(jsonMap map[string]interface{},
+	metricName []string, metricType string) []metric.Metric {
+
+	results := []metric.Metric{}
+
+	if _, ok := jsonMap["count"]; ok {
+		for key, value := range jsonMap {
+			if key == "type" {
+				continue
+			}
+
+			metricType := MetricTypeGauge
+			if key == "count" {
+				metricType = MetricTypeCounter
+			}
+
+			compositeMetricName := strings.Join(metricName, ".")
+			m, ok := createMetricFromDatam(key, value, compositeMetricName, metricType)
+			if ok {
+				results = append(results, m)
+			}
+		}
+	}
+	return results
+}
+
+func collectCounter(jsonMap map[string]interface{}, metricName []string,
+	metricType string) []metric.Metric {
+
+	if _, ok := jsonMap["count"]; ok {
+		compositeMetricName := strings.Join(metricName, ".")
+		return metricFromMap(&jsonMap, compositeMetricName, metricType)
+	}
+	return []metric.Metric{}
+}
+
+func collectRate(jsonMap map[string]interface{}, metricName []string) []metric.Metric {
+	results := []metric.Metric{}
+	if unit, ok := jsonMap["unit"]; ok && (unit == "seconds" || unit == "milliseconds") {
+		for key, value := range jsonMap {
+			if key == "unit" {
+				continue
+			}
+			metricType := MetricTypeGauge
+
+			if key == "count" {
+				metricType = MetricTypeCounter
+			}
+
+			compositeMetricName := strings.Join(metricName, ".")
+			m, ok := createMetricFromDatam(key, value, compositeMetricName, metricType)
+			if ok {
+				results = append(results, m)
+			}
+		}
+
+	}
+	return results
+}
+
+func collectMeter(jsonMap map[string]interface{}, metricName []string) []metric.Metric {
+	results := []metric.Metric{}
+
+	if checkForMeterUnits(jsonMap) {
+		for key, value := range jsonMap {
+			if key == "unit" || key == "event_type" || key == "type" {
+				continue
+			}
+
+			metricType := MetricTypeGauge
+			if key == "count" {
+				metricType = MetricTypeCounter
+			}
+
+			compositeMetricName := strings.Join(metricName, ".")
+			m, ok := createMetricFromDatam(key, value, compositeMetricName, metricType)
+			if ok {
+				results = append(results, m)
+			}
+		}
+	}
+
+	return results
+}
+
+func checkForMeterUnits(jsonMap map[string]interface{}) bool {
+	if _, ok := jsonMap["event_type"]; ok {
+		if unit, ok := jsonMap["unit"]; ok &&
+			(unit == "seconds" || unit == "milliseconds" || unit == "minutes") {
+			return true
+		}
+	}
+	return false
 }
--- src/fullerite/collector/nerve_uwsgi_test.go
+++ src/fullerite/collector/nerve_uwsgi_test.go
@@ -176,6 +176,32 @@ func parseURL(url string) (string, string) {
 	return ip, port
 }
 
+func extractMetricWithName(metrics []metric.Metric,
+	metricName string) (metric.Metric, bool) {
+	var m metric.Metric
+
+	for _, metric := range metrics {
+		if metric.Name == metricName {
+			return metric, true
+		}
+	}
+
+	return m, false
+}
+
+func extractMetricWithType(metrics []metric.Metric,
+	metricType string) (metric.Metric, bool) {
+	var m metric.Metric
+
+	for _, metric := range metrics {
+		if metric.MetricType == metricType {
+			return metric, true
+		}
+	}
+
+	return m, false
+}
+
 func getTestNerveUWSGI() *nerveUWSGICollector {
 	return newNerveUWSGICollector(make(chan metric.Metric), 12, l.WithField("testing", "nerveuwsgi"))
 }
@@ -417,4 +443,171 @@ func TestNonConflictingServiceQueries(t *testing.T) {
 	validateUWSGIResults(t, actual)
 	validateFullDimensions(t, actual, "test_service", goodPort)
 	validateEmptyChannel(t, inst.Channel())
+}
+
+func TestDropwizardCounter(t *testing.T) {
+	rawData := []byte(`
+{
+  "jetty": {
+     "percent": {
+         "foo": {
+            "active-requests": {
+              "count": 0,
+              "type": "counter"
+            }
+         }
+     }
+   }
+}
+        `)
+
+	metrics, err := parseUWSGIMetrics(&rawData)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(metrics))
+}
+
+func TestInvalidDropwizard(t *testing.T) {
+	rawData := []byte(`
+{
+        "meters": {
+            "pyramid_uwsgi_metrics.tweens.2xx-responses": {
+                "units": "events/second"
+            }
+        }
+}
+        `)
+
+	metrics, err := parseUWSGIMetrics(&rawData)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(metrics))
+}
+
+func TestDropJVMMetrics(t *testing.T) {
+	rawData := []byte(`
+{
+  "jvm": {
+    "current_time": 1447699597200,
+    "uptime": 419631,
+    "thread_count": 315,
+    "vm": {
+      "version": "1.8.0_45-b14",
+      "name": "Java HotSpot(TM) 64-Bit Server VM"
+    },
+    "garbage-collectors": {
+      "ConcurrentMarkSweep": {
+        "runs": 13,
+        "time": 1531
+      },
+      "ParNew": {
+        "runs": 45146,
+        "time": 1324093
+      }
+    },
+    "daemon_thread_count": 96,
+    "thread-states": {
+      "terminated": 0,
+      "runnable": 0.17777777777777778,
+      "timed_waiting": 0.7714285714285715,
+      "waiting": 0.050793650793650794,
+      "new": 0,
+      "blocked": 0
+    }
+  }
+}
+        `)
+
+	metrics, err := parseUWSGIMetrics(&rawData)
+	assert.Nil(t, err)
+	assert.Equal(t, 14, len(metrics))
+}
+
+func TestDropwizardTimer(t *testing.T) {
+	rawData := []byte(`
+{
+  "jetty": {
+    "trace-requests": {
+      "duration": {
+        "p98": 0,
+        "p99": 0,
+        "unit": "milliseconds",
+        "mean": 0
+      },
+      "rate": {
+        "count": 0,
+        "m5": 0,
+        "m15": 0,
+        "m1": 0,
+        "unit": "seconds",
+        "mean": 0
+      },
+      "type": "timer"
+    },
+    "foo": {
+      "type": "gauge",
+      "value": 5.612
+    }
+  }
+}
+        `)
+	metrics, err := parseUWSGIMetrics(&rawData)
+	assert.Nil(t, err)
+	assert.Equal(t, 9, len(metrics))
+	t.Log(metrics)
+}
+
+func TestDropwizardGauge(t *testing.T) {
+	rawData := []byte(`
+{
+  "org.eclipse.jetty.servlet.ServletContextHandler": {
+    "percent-4xx-1m": {
+      "type": "gauge",
+      "value": 5.611051195902441e-77
+    }
+  }
+}
+        `)
+	metrics, err := parseUWSGIMetrics(&rawData)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(metrics))
+}
+
+func TestDropwizardJsonInput(t *testing.T) {
+	dat, err := ioutil.ReadFile("sample.json")
+
+	assert.Nil(t, err)
+
+	metrics, err := parseUWSGIMetrics(&dat)
+	assert.Nil(t, err)
+	assert.Equal(t, 560, len(metrics))
+}
+
+func TestDropwizardHistogram(t *testing.T) {
+	rawData := []byte(`
+{
+  "foo": {
+    "bar": {
+      "type": "histogram",
+      "count": 100,
+      "min": 2,
+      "max": 2,
+      "mean": 2,
+      "std_dev": 0,
+      "median": 2,
+      "p75": 2,
+      "p95": 2,
+      "p98": 2,
+      "p99": 2,
+      "p999": 2
+    }
+  }
+}
+        `)
+	metrics, err := parseUWSGIMetrics(&rawData)
+	assert.Nil(t, err)
+	assert.Equal(t, 11, len(metrics))
+	t.Log(len(metrics))
+	counterMetric, ok := extractMetricWithType(metrics, "COUNTER")
+	assert.True(t, ok)
+
+	assert.Equal(t, 100.0, counterMetric.Value)
 }
