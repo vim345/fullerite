35687c09f8a1fdd24460766b29b862082ac1aae6
T SHEASHA <tsheasha@yelp.com>
2015-11-17 18:23:57 -0800

    Remove unecessary pemrission check

--- src/diamond/collectors/numazoneinfo/numazoneinfo.py
+++ src/diamond/collectors/numazoneinfo/numazoneinfo.py
@@ -14,6 +14,7 @@ from re import compile as re_compile
 
 import diamond.collector
 
+ZONEINFO_PATH = '/proc/zoneinfo'
 node_re = re_compile(r'^Node\s+(?P<node>\d+),\s+zone\s+(?P<zone>\w+)$')
 
 
@@ -25,7 +26,7 @@ class NUMAZoneInfoCollector(diamond.collector.Collector):
         """
         config = super(NUMAZoneInfoCollector, self).get_default_config()
         config.update({
-            'path': '/proc/zoneinfo',
+            'path': ZONEINFO_PATH,
         })
         return config
 
@@ -33,37 +34,32 @@ class NUMAZoneInfoCollector(diamond.collector.Collector):
         try:
             filepath = self.config['path']
 
-            if not os.access(filepath, os.R_OK):
-                self.log.error('Permission to access %s denied', filepath)
-                return None
+            with open(filepath, 'r') as file_handle:
+                metric = ''
+                numlines_to_process = 0
 
-            file_handle = open(filepath, 'r')
-            metric = ''
-            numlines_to_process = 0
+                for line in file_handle:
+                    match = node_re.match(line)
 
-            for line in file_handle:
-                match = node_re.match(line)
+                    if numlines_to_process > 0:
+                        numlines_to_process -= 1
+                        statname, metric_value = line.split('pages')[-1].split()
+                        metric_name = ''.join([metric, statname])
 
-                if numlines_to_process > 0:
-                    numlines_to_process -= 1
-                    statname, metric_value = line.split('pages')[-1].split()
-                    metric_name = ''.join([metric, statname])
+                        self.publish(metric_name, metric_value)
 
-                    self.publish(metric_name, metric_value)
+                    if match:
+                        self.log.debug("Matched: %s %s" %
+                                      (match.group('node'), match.group('zone')))
 
-                if match:
-                    self.log.debug("Matched: %s %s" %
-                                  (match.group('node'), match.group('zone')))
+                        node = match.group('node') or ''
+                        zone = match.group('zone') or ''
+                        metric = "node{0!s}-zone-{0!s}-".format(node, zone)
 
-                    node = match.group('node') or ''
-                    zone = match.group('zone') or ''
-                    metric = "node{0!s}-zone-{0!s}-".format(node, zone)
+                        # We get 4 lines afterwards for free, min, low, and high
+                        # page thresholds
+                        numlines_to_process = 4
 
-                    # We get 4 lines afterwards for free, min, low, and high
-                    # page thresholds
-                    numlines_to_process = 4
-
-            file_handle.close()
         except Exception as e:
             self.log.error('Failed because: %s' % str(e))
             return None
