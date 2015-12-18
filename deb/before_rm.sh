bfa73b60e9b58202a6024c697155044706d5ff0a
T SHEASHA <tsheasha@yelp.com>
2015-12-17 19:06:14 -0800

    Remove the belongings of fuller not fullerite

--- deb/before_rm.sh
+++ deb/before_rm.sh
@@ -3,8 +3,8 @@
 service 'fullerite' stop
 service 'fullerite_diamond_server' stop
 
-if [ "$(id 'fullerite')" ]; then
-  userdel 'fullerite'
+if [ "$(id 'fuller')" ]; then
+  userdel 'fuller'
 fi
 
 rm -rf /var/log/fullerite
