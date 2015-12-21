2921f2d1e2118a5d40204fa8a308752514122253
T SHEASHA <tsheasha@yelp.com>
2015-12-21 08:25:47 -0800

    No counter just cumulative

--- src/diamond/collectors/tcp/tcp.py
+++ src/diamond/collectors/tcp/tcp.py
@@ -277,4 +277,4 @@ class TCPCollector(diamond.collector.Collector):
                 self.publish_gauge(metric_name, value, 0)
             else:
                 metric_name = '.'.join(['tcp', metric_name])
-                self.publish_counter(metric_name, value, 0)
+                self.publish_cumulative_counter(metric_name, value)
