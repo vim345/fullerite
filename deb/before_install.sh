9dd4484934bc2a85f964fcabd06de89a0ca6631b
T SHEASHA <tsheasha@yelp.com>
2016-01-19 04:36:23 -0800

    Using getent

--- deb/before_install.sh
+++ deb/before_install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 if ! [ "$(id 'fuller')" ]; then
-  if [ "$(grep -q "\bfuller\b" /etc/group)" ]; then
+  getent group 'fuller' > /dev/null 2>&1
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
     echo "creating user fuller and adding to fuller group"
     useradd --no-create-home --system -g "fuller"
-  else
+  elif [ $exit_code -eq 2 ]; then
     echo "creating user and group fuller"
     useradd --no-create-home --system --user-group "fuller"
+  else
+    echo "could not get group info, failed"
+    exit 1
   fi
 fi
 
