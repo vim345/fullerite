682c355112ab377324e67e103b0afbd89abe26c9
T SHEASHA <tsheasha@yelp.com>
2016-01-19 03:24:07 -0800

    Add a new group only if one does not exist

--- deb/before_install.sh
+++ deb/before_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 if ! [ "$(id 'fuller')" ]; then
-  echo "creating user fuller"
-  useradd --no-create-home --system --user-group "fuller"
+  if [ "$(grep -q "\bfuller\b" /etc/group)" ]; then
+    echo "creating user fuller and adding to fuller group"
+    useradd --no-create-home --system -g "fuller"
+  else
+    echo "creating user and group fuller"
+    useradd --no-create-home --system --user-group "fuller"
+  fi
 fi
 
 echo "creating log directory: /var/log/fullerite"
