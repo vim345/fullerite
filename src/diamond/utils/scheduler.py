50c968e336e0b2de79b9efaa5e2d562a8ac7d1ed
Hemant Kumar <gethemant@gmail.com>
2016-02-24 08:14:34 -0500

    Use collector logger for logging warnings
    about collection timing out, so as we can capture name of the
    collector

--- src/diamond/utils/scheduler.py
+++ src/diamond/utils/scheduler.py
@@ -110,14 +110,14 @@ def collector_process(collector, log):
             }
             collector.publish('fullerite.collection_time_exceeded', 1)
             try:
-                log.error('%s took too long to run! Killed!', collector.name)
+                collector.log.warn("Took too long to run, Killed!")
                 children = get_children(pid)
                 for child in children:
                     os.kill(int(child), signal.SIGKILL)
             except OSError as e:
                 log.debug('Process died on its own!')
             except Exception as e:
-                log.exception('%s Killing children failed', collector.name)
+                collector.log.warn("Killing children failed")
 
         except SIGHUPException:
             # Reload the config if requested
