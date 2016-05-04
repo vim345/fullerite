d92bd7b150ea6c04487ddadfe2326d0e173c3216
Hemant Kumar <gethemant@gmail.com>
2016-05-04 18:59:01 -0400

    Add more code for NerveHTTPD collector

--- src/fullerite/collector/nerve_httpd.go
+++ src/fullerite/collector/nerve_httpd.go
@@ -2,6 +2,7 @@ package collector
 
 import (
 	"fullerite/metric"
+	"time"
 
 	l "github.com/Sirupsen/logrus"
 )
@@ -10,6 +11,11 @@ import (
 // and reports metric for them
 type NerveHTTPD struct {
 	baseCollector
+
+	configFilePath string
+	queryPath      string
+	timeout        int
+	statusTTL      time.Duration
 }
 
 func init() {
@@ -23,12 +29,30 @@ func newNerveHTTPD(channel chan metric.Metric, initialInterval int, log *l.Entry
 	c.log = log
 
 	c.name = collectorName
+	c.configFilePath = "/etc/nerve/nerve.conf.json"
+	c.queryPath = "server-status?auto"
+	c.timeout = 2
+	c.statusTTL = time.Duration(60) * time.Minute
+
 	return c
 }
 
 // Configure the collector
 func (c *NerveHTTPD) Configure(configMap map[string]interface{}) {
+	if val, exists := configMap["queryPath"]; exists {
+		c.queryPath = val.(string)
+	}
+	if val, exists := configMap["configFilePath"]; exists {
+		c.configFilePath = val.(string)
+	}
 
+	if val, exists := configMap["status_ttl"]; exists {
+		if t, ok := val.(int); ok {
+			c.statusTTL = time.Duration(t) * time.Second
+		}
+	}
+
+	c.configureCommonParams(configMap)
 }
 
 // Collect the metrics
