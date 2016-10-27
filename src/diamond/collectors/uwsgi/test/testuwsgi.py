f1dd36ccd5f9e0561175d38a8786c90405b3e569
Anthony Steinhauser <ast@yelp.com>
2016-10-27 10:43:59 -0700

    uWSGI stats server collector

---
+++ src/diamond/collectors/uwsgi/test/testuwsgi.py
@@ -0,0 +1,311 @@
+#!/usr/bin/python
+# coding=utf-8
+################################################################################
+
+from test import CollectorTestCase
+from test import get_collector_config
+from test import unittest
+from mock import Mock
+from mock import patch
+
+from diamond.collector import Collector
+from uwsgi import UwsgiCollector
+import httplib
+
+################################################################################
+
+
+class TestHTTPResponse(httplib.HTTPResponse):
+    def __init__(self):
+        pass
+
+    def read(self):
+        pass
+
+
+class TestUwsgiCollector(CollectorTestCase):
+    def setUp(self, config=None):
+        if config is None:
+            config = get_collector_config('UwsgiCollector', {
+                'interval': '10',
+                'url': 'http://www.example.com:80/'
+            })
+        else:
+            config = get_collector_config('UwsgiCollector', config)
+
+        self.collector = UwsgiCollector(config, configfile=config['collectors']['UwsgiCollector'])
+
+        self.HTTPResponse = TestHTTPResponse()
+
+        httplib.HTTPConnection.request = Mock(return_value=True)
+        httplib.HTTPConnection.getresponse = Mock(
+            return_value=self.HTTPResponse)
+
+    def test_import(self):
+        self.assertTrue(UwsgiCollector)
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_synthetic_data(self, publish_mock):
+        self.setUp()
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-fake-1').getvalue()))
+
+        patch_headers = patch.object(
+            TestHTTPResponse,
+            'getheaders',
+            Mock(return_value={}))
+
+        patch_headers.start()
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+
+        self.assertPublishedMany(publish_mock, {
+            'IdleWorkers': 0,
+            'BusyWorkers': 0,
+            'SigWorkers': 0,
+            'CheapWorkers': 0,
+            'PauseWorkers': 0,
+            'UnknownStateWorkers': 0,
+        })
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-fake-2').getvalue()))
+
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+
+        self.assertPublishedMany(publish_mock, {
+            'IdleWorkers': 1,
+            'BusyWorkers': 3,
+            'SigWorkers': 1,
+            'CheapWorkers': 1,
+            'PauseWorkers': 1,
+            'UnknownStateWorkers': 0,
+        })
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-fake-3').getvalue()))
+
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+        patch_headers.stop()
+
+        self.assertPublishedMany(publish_mock, {
+            'IdleWorkers': 0,
+            'BusyWorkers': 1,
+            'SigWorkers': 0,
+            'CheapWorkers': 0,
+            'PauseWorkers': 0,
+            'UnknownStateWorkers': 1,
+        })
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_real_data(self, publish_mock):
+        self.setUp()
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-live-1').getvalue()))
+
+        patch_headers = patch.object(
+            TestHTTPResponse,
+            'getheaders',
+            Mock(return_value={}))
+
+        patch_headers.start()
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+
+        self.assertPublishedMany(publish_mock, {
+            'IdleWorkers': 23,
+            'BusyWorkers': 0,
+            'SigWorkers': 0,
+            'CheapWorkers': 0,
+            'PauseWorkers': 0,
+            'UnknownStateWorkers': 0,
+        })
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-live-2').getvalue()))
+
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+        patch_headers.stop()
+
+        self.assertPublishedMany(publish_mock, {
+            'IdleWorkers': 21,
+            'BusyWorkers': 2,
+            'SigWorkers': 0,
+            'CheapWorkers': 0,
+            'PauseWorkers': 0,
+            'UnknownStateWorkers': 0,
+        })
+
+    @patch.object(Collector, 'publish')
+    def test_should_work_with_multiple_servers(self, publish_mock):
+        self.setUp(config={
+            'urls': [
+                'nickname1 http://localhost:2081/',
+                'nickname2 http://localhost:2081/',
+            ],
+        })
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-live-1').getvalue()))
+
+        patch_headers = patch.object(
+            TestHTTPResponse,
+            'getheaders',
+            Mock(return_value={}))
+
+        patch_headers.start()
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+
+        self.assertPublishedMany(publish_mock, {})
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-live-4').getvalue()))
+
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+        patch_headers.stop()
+
+        metrics = {
+            'nickname1.IdleWorkers': 12,
+            'nickname1.BusyWorkers': 11,
+            'nickname1.SigWorkers': 0,
+            'nickname1.CheapWorkers': 0,
+            'nickname1.PauseWorkers': 0,
+            'nickname1.UnknownStateWorkers': 0,
+
+            'nickname2.IdleWorkers': 12,
+            'nickname2.BusyWorkers': 11,
+            'nickname2.SigWorkers': 0,
+            'nickname2.CheapWorkers': 0,
+            'nickname2.PauseWorkers': 0,
+            'nickname2.UnknownStateWorkers': 0,
+        }
+
+        self.setDocExample(collector=self.collector.__class__.__name__,
+                           metrics=metrics,
+                           defaultpath=self.collector.config['path'])
+        self.assertPublishedMany(publish_mock, metrics)
+
+    @patch.object(Collector, 'publish')
+    def test_sig0(self, publish_mock):
+        self.setUp(config={
+            'urls': 'vhost http://localhost/server-status?auto',
+        })
+
+        patch_read = patch.object(
+            TestHTTPResponse,
+            'read',
+            Mock(return_value=self.getFixture(
+                'status-json-live-3').getvalue()))
+
+        patch_headers = patch.object(
+            TestHTTPResponse,
+            'getheaders',
+            Mock(return_value={}))
+
+        patch_headers.start()
+        patch_read.start()
+        self.collector.collect()
+        patch_read.stop()
+
+        self.assertPublishedMany(publish_mock, {
+            'vhost.IdleWorkers': 22,
+            'vhost.BusyWorkers': 0,
+            'vhost.SigWorkers': 1,
+            'vhost.CheapWorkers': 0,
+            'vhost.PauseWorkers': 0,
+            'vhost.UnknownStateWorkers': 0,
+        })
+
+    @patch.object(Collector, 'publish')
+    def test_issue_533(self, publish_mock):
+        self.setUp(config={
+            'urls': 'localhost http://localhost:80/server-status?auto,',
+        })
+
+        expected_urls = {'localhost': 'http://localhost:80/server-status?auto'}
+
+        self.assertEqual(self.collector.urls, expected_urls)
+
+    @patch.object(Collector, 'publish')
+    def test_url_with_port(self, publish_mock):
+        self.setUp(config={
+            'urls': 'localhost http://localhost:80/server-status?auto',
+        })
+
+        expected_urls = {'localhost': 'http://localhost:80/server-status?auto'}
+
+        self.assertEqual(self.collector.urls, expected_urls)
+
+    @patch.object(Collector, 'publish')
+    def test_url_without_port(self, publish_mock):
+        self.setUp(config={
+            'urls': 'localhost http://localhost/server-status?auto',
+        })
+
+        expected_urls = {'localhost': 'http://localhost/server-status?auto'}
+
+        self.assertEqual(self.collector.urls, expected_urls)
+
+    @patch.object(Collector, 'publish')
+    def test_url_without_nickname(self, publish_mock):
+        self.setUp(config={
+            'urls': 'http://localhost/server-status?auto',
+        })
+
+        expected_urls = {'': 'http://localhost/server-status?auto'}
+
+        self.assertEqual(self.collector.urls, expected_urls)
+
+    @patch.object(Collector, 'publish')
+    def test_issue_538(self, publish_mock):
+        self.setUp(config={
+            'enabled': True,
+            'path_suffix': "",
+            'ttl_multiplier': 2,
+            'measure_collector_time': False,
+            'byte_unit': 'byte',
+            'urls': 'localhost http://localhost:80/server-status?auto',
+        })
+
+        expected_urls = {'localhost': 'http://localhost:80/server-status?auto'}
+
+        self.assertEqual(self.collector.urls, expected_urls)
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
