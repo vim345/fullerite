f9ac5b7f49aa1976e76c5327145318b5831e72b1
Anthony Steinhauser <ast@yelp.com>
2016-11-09 06:38:28 -0800

    Go collector for uWSGI stats

---
+++ src/fullerite/collector/nerve_uwsgi_stats.go
@@ -0,0 +1,184 @@
+package collector
+
+import (
+	"fullerite/config"
+	"fullerite/metric"
+	"fullerite/util"
+
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	l "github.com/Sirupsen/logrus"
+)
+
+type nerveUWSGIStatsCollector struct {
+	baseCollector
+
+	configFilePath    string
+	queryPath         string
+	timeout           int
+	servicesWhitelist []string
+}
+
+func init() {
+	RegisterCollector("NerveUWSGIStats", newNerveUWSGIStats)
+}
+
+func newNerveUWSGIStats(channel chan metric.Metric, initialInterval int, log *l.Entry) Collector {
+	col := new(nerveUWSGIStatsCollector)
+
+	col.log = log
+	col.channel = channel
+	col.interval = initialInterval
+
+	col.name = "NerveUWSGIStats"
+	col.configFilePath = "/etc/nerve/nerve.conf.json"
+	col.queryPath = "status/uwsgi"
+	col.timeout = 2
+
+	return col
+}
+
+func (n *nerveUWSGIStatsCollector) Configure(configMap map[string]interface{}) {
+	if val, exists := configMap["queryPath"]; exists {
+		n.queryPath = val.(string)
+	}
+	if val, exists := configMap["configFilePath"]; exists {
+		n.configFilePath = val.(string)
+	}
+	if val, exists := configMap["servicesWhitelist"]; exists {
+		n.servicesWhitelist = config.GetAsSlice(val)
+	}
+
+	if val, exists := configMap["http_timeout"]; exists {
+		n.timeout = config.GetAsInt(val, 2)
+	}
+
+	n.configureCommonParams(configMap)
+}
+
+func (n *nerveUWSGIStatsCollector) Collect() {
+	rawFileContents, err := ioutil.ReadFile(n.configFilePath)
+	if err != nil {
+		n.log.Warn("Failed to read the contents of file ", n.configFilePath, " because ", err)
+		return
+	}
+
+	services, err := util.ParseNerveConfig(&rawFileContents, false)
+	if err != nil {
+		n.log.Warn("Failed to parse the nerve config at ", n.configFilePath, ": ", err)
+		return
+	}
+	n.log.Debug("Finished parsing Nerve config into ", services)
+
+	for _, service := range services {
+		go n.queryService(service.Name, service.Port)
+	}
+}
+
+func (n *nerveUWSGIStatsCollector) queryService(serviceName string, port int) {
+	serviceLog := n.log.WithField("service", serviceName)
+
+	endpoint := fmt.Sprintf("http://localhost:%d/%s", port, n.queryPath)
+	serviceLog.Debug("making GET request to ", endpoint)
+
+	rawResponse, err := readJSONFromEndpoint(endpoint, n.timeout)
+	if err != nil {
+		serviceLog.Warn("Failed to query endpoint ", endpoint, ": ", err)
+		return
+	}
+	metrics, err := parseJSONData(rawResponse, n.serviceInWhitelist(serviceName))
+	if err != nil {
+		serviceLog.Warn("Failed to parse response into metrics: ", err)
+		return
+	}
+
+	metric.AddToAll(&metrics, map[string]string{
+		"service": serviceName,
+		"port":    strconv.Itoa(port),
+	})
+	serviceLog.Debug("Sending ", len(metrics), " to channel")
+	for _, m := range metrics {
+		n.Channel() <- m
+	}
+}
+
+func parseJSONData(raw []byte, ccEnabled bool) ([]metric.Metric, error) {
+	result := make(map[string]interface{})
+	err := json.Unmarshal(raw, &result)
+	results := []metric.Metric{}
+	if err != nil {
+		return results, err
+	}
+	registry := make(map[string]int)
+	registry["IdleWorkers"] = 0
+	registry["BusyWorkers"] = 0
+	registry["SigWorkers"] = 0
+	registry["PauseWorkers"] = 0
+	registry["CheapWorkers"] = 0
+	registry["UnknownStateWorkers"] = 0
+	for _, worker := range result["workers"].([]interface{}) {
+		status := worker.(map[string]interface{})["status"].(string)
+		if strings.Index(status, "sig") == 0 {
+			status = "sig"
+		}
+		status = strings.Title(status) + "Workers"
+		_, exists := registry[status]
+		if !exists {
+			status = "UnknownStateWorkers"
+		}
+		registry[status]++
+	}
+	for key, value := range registry {
+		results = append(results, metric.WithValue(key, float64(value)))
+	}
+	return results, err
+}
+
+func readJSONFromEndpoint(endpoint string, timeout int) ([]byte, error) {
+	client := http.Client{
+		Timeout: time.Duration(timeout) * time.Second,
+	}
+
+	rsp, err := client.Get(endpoint)
+
+	if rsp != nil {
+		defer func() {
+			io.Copy(ioutil.Discard, rsp.Body)
+			rsp.Body.Close()
+		}()
+	}
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	if rsp != nil && rsp.StatusCode != 200 {
+		err := fmt.Errorf("%s returned %d error code", endpoint, rsp.StatusCode)
+		return []byte{}, err
+	}
+
+	txt, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return txt, nil
+}
+
+// serviceInWhitelist returns true if the service name passed as argument
+// is found among the ones whitelisted by the user
+func (n *nerveUWSGIStatsCollector) serviceInWhitelist(service string) bool {
+	for _, s := range n.servicesWhitelist {
+		if s == service {
+			return true
+		}
+	}
+	return false
+}
---
+++ src/fullerite/collector/nerve_uwsgi_stats_test.go
@@ -0,0 +1,478 @@
+package collector
+
+import (
+	"fullerite/metric"
+	"fullerite/util"
+
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	l "github.com/Sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func getNerveConfigForTest() []byte {
+	raw := `
+	{
+		"heartbeat_path": "/var/run/nerve/heartbeat",
+		"instance_id": "srv1-devc",
+		"services": {
+		"example_service.main.norcal-devc.superregion:norcal-devc.13752.new": {
+			"check_interval": 7,
+			"checks": [
+			{
+				"fall": 2,
+				"headers": {},
+				"host": "127.0.0.1",
+				"open_timeout": 6,
+				"port": 6666,
+				"rise": 1,
+				"timeout": 6,
+				"type": "http",
+				"uri": "/http/example_service.main/13752/status"
+			}
+			],
+			"host": "10.56.5.21",
+			"port": 13752,
+			"weight": 24,
+			"zk_hosts": [
+			"10.40.5.5:22181",
+			"10.40.5.6:22181",
+			"10.40.1.17:22181"
+			],
+			"zk_path": "/nerve/superregion:norcal-devc/example_service.main"
+		},
+		"example_service.mesosstage_main.norcal-devc.superregion:norcal-devc.13752.new": {
+			"check_interval": 7,
+			"checks": [
+			{
+				"fall": 2,
+				"headers": {},
+				"host": "127.0.0.1",
+				"open_timeout": 6,
+				"port": 6666,
+				"rise": 1,
+				"timeout": 6,
+				"type": "http",
+				"uri": "/http/example_service.mesosstage_main/13752/status"
+			}
+			],
+			"host": "10.56.5.21",
+			"port": 22222,
+			"weight": 24,
+			"zk_hosts": [
+			"10.40.5.5:22181",
+			"10.40.5.6:22181",
+			"10.40.1.17:22181"
+			],
+			"zk_path": "/nerve/superregion:norcal-devc/example_service.mesosstage_main"
+		}
+		}
+	}
+	`
+	return []byte(raw)
+}
+
+func getTestUWSGIStatsResponse() string {
+	return `{
+	"version":"XXXXXXXXXXXX",
+	"listen_queue":0,
+	"listen_queue_errors":0,
+	"signal_queue":0,
+	"load":0,
+	"pid":104912,
+	"uid":33,
+	"gid":33,
+	"cwd":"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	"locks":[
+	{
+		"user 0":0
+	},
+	{
+		"signal":0
+	},
+	{
+		"filemon":0
+	},
+	{
+		"timer":0
+	},
+	{
+		"rbtimer":0
+	},
+	{
+		"cron":0
+	},
+	{
+		"rpc":0
+	},
+	{
+		"snmp":0
+	},
+	{
+		"cache_healthcheck":0
+	}
+	],
+	"caches":[
+	{
+		"name":"healthcheck",
+		"hash":"djb33x",
+		"hashsize":65536,
+		"keysize":2048,
+		"max_items":32,
+		"blocks":32,
+		"blocksize":65536,
+		"items":0,
+		"hits":0,
+		"miss":0,
+		"full":0,
+		"last_modified_at":0
+	}
+	],
+	"sockets":[
+	{
+		"name":"127.0.0.1:2000",
+		"proto":"uwsgi",
+		"queue":0,
+		"max_queue":100,
+		"shared":0,
+		"can_offload":0
+	}
+	],
+	"workers":[
+	{
+		"id":1,
+		"pid":79545,
+		"accepting":1,
+		"requests":1895,
+		"delta_requests":345,
+		"exceptions":0,
+		"harakiri_count":0,
+		"signals":300,
+		"signal_queue":0,
+		"status":"idle",
+		"rss":789790720,
+		"vsz":1477410816,
+		"running_time":19757779,
+		"last_spawn":1477079439,
+		"respawn_count":5,
+		"tx":2244564,
+		"avg_rt":182085,
+		"apps":[
+		{
+			"id":0,
+			"modifier1":0,
+			"mountpoint":"",
+			"startup_time":23,
+			"requests":1895,
+			"exceptions":0,
+			"chdir":""
+		}
+		],
+		"cores":[
+		{
+			"id":0,
+			"requests":1895,
+			"static_requests":0,
+			"routed_requests":0,
+			"offloaded_requests":0,
+			"write_errors":0,
+			"read_errors":0,
+			"in_request":0,
+			"vars":[
+
+			],
+			"req_info":
+			{
+
+			}
+		}
+		]
+	},
+	{
+		"id":2,
+		"pid":3930,
+		"accepting":1,
+		"requests":2419,
+		"delta_requests":290,
+		"exceptions":0,
+		"harakiri_count":0,
+		"signals":300,
+		"signal_queue":0,
+		"status":"busy",
+		"rss":827023360,
+		"vsz":1514618880,
+		"running_time":36199811,
+		"last_spawn":1477081120,
+		"respawn_count":5,
+		"tx":6627598,
+		"avg_rt":178958,
+		"apps":[
+		{
+			"id":0,
+			"modifier1":0,
+			"mountpoint":"",
+			"startup_time":23,
+			"requests":2420,
+			"exceptions":0,
+			"chdir":""
+		}
+		],
+		"cores":[
+		{
+			"id":0,
+			"requests":2419,
+			"static_requests":0,
+			"routed_requests":0,
+			"offloaded_requests":0,
+			"write_errors":0,
+			"read_errors":0,
+			"in_request":1,
+			"vars":[
+"SCRIPT_URL=/",
+"SCRIPT_URI=XXXXXXXXXXXXXXXXXXXXX",
+"PATH_INFO=/",
+"HTTP_USER_AGENT=XXXXXXXXXXXXXXXXXXXXXXX",
+"HTTP_ACCEPT=*/*",
+"HTTP_HOST=XXXXXXXXXXXXXXXXXXXXXXXX",
+"HTTP_X_MODE=ro",
+"HTTP_X_FORWARDED_FOR=XXXXXXXXXXXXXXXXXXXXX",
+"HTTP_CONNECTION=close",
+"PATH=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+"SERVER_SIGNATURE=",
+"SERVER_SOFTWARE=XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+"SERVER_NAME=XXXXXXXXXXXX",
+"SERVER_ADDR=XXXXXXXXXXXXXXXXXX",
+"SERVER_PORT=80",
+"REMOTE_ADDR=XXXXXXXXXXXXXXXX",
+"DOCUMENT_ROOT=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+"SERVER_ADMIN=XXXXXXXXXXXXXXXXX",
+"SCRIPT_FILENAME=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+"REMOTE_PORT=XXXXX",
+"GATEWAY_INTERFACE=CGI/1.1",
+"SERVER_PROTOCOL=HTTP/1.1",
+"REQUEST_METHOD=GET",
+"QUERY_STRING=",
+"REQUEST_URI=XXXXXXXXXX",
+"SCRIPT_NAME=",
+"BODY_SIZE=0",
+"BODY_SIZE=0",
+""
+			],
+			"req_info":
+			{
+			"request_start":1477081161
+			}
+		}
+		]
+	}
+	]
+	}
+	`
+}
+
+func validateUWSGIStatsResults(t *testing.T, actual []metric.Metric) {
+	assert.Equal(t, 6, len(actual))
+
+	for _, m := range actual {
+
+		switch m.Name {
+		case "IdleWorkers":
+			assert.Equal(t, 1.0, m.Value)
+			assert.Equal(t, metric.Gauge, m.MetricType)
+		case "BusyWorkers":
+			assert.Equal(t, 1.0, m.Value)
+			assert.Equal(t, metric.Gauge, m.MetricType)
+		case "SigWorkers":
+			assert.Equal(t, 0.0, m.Value)
+			assert.Equal(t, metric.Gauge, m.MetricType)
+		case "CheapWorkers":
+			assert.Equal(t, 0.0, m.Value)
+			assert.Equal(t, metric.Gauge, m.MetricType)
+		case "PauseWorkers":
+			assert.Equal(t, 0.0, m.Value)
+			assert.Equal(t, metric.Gauge, m.MetricType)
+		case "UnknownStateWorkers":
+			assert.Equal(t, 0.0, m.Value)
+			assert.Equal(t, metric.Gauge, m.MetricType)
+		}
+	}
+}
+
+func validateStatsDimensions(t *testing.T, actual []metric.Metric, serviceName, port string) {
+	for _, m := range actual {
+		assert.Equal(t, 2, len(m.Dimensions))
+
+		val, exists := m.GetDimensionValue("service")
+		assert.True(t, exists)
+		assert.Equal(t, serviceName, val)
+
+		val, exists = m.GetDimensionValue("port")
+		assert.True(t, exists)
+		assert.Equal(t, port, val)
+	}
+
+}
+
+func validateStatsEmptyChannel(t *testing.T, c chan metric.Metric) {
+	close(c)
+	for m := range c {
+		t.Fatal("The channel was not empty! got value ", m)
+	}
+}
+
+func getTestNerveUWSGIStats() *nerveUWSGIStatsCollector {
+	return newNerveUWSGIStats(make(chan metric.Metric), 12, l.WithField("testing", "nerveuwsgistats")).(*nerveUWSGIStatsCollector)
+}
+
+func TestDefaultConfigNerveUWSGIStats(t *testing.T) {
+	inst := getTestNerveUWSGIStats()
+	inst.Configure(make(map[string]interface{}))
+
+	assert.Equal(t, 12, inst.Interval())
+	assert.Equal(t, 2, inst.timeout)
+	assert.Equal(t, "/etc/nerve/nerve.conf.json", inst.configFilePath)
+	assert.Equal(t, "status/uwsgi", inst.queryPath)
+}
+
+func TestConfigNerveUWSGIStats(t *testing.T) {
+	cfg := map[string]interface{}{
+		"interval":       345,
+		"configFilePath": "/etc/your/moms/house",
+		"queryPath":      "littlepiggies",
+		"http_timeout":   12,
+	}
+
+	inst := getTestNerveUWSGIStats()
+	inst.Configure(cfg)
+
+	assert.Equal(t, 345, inst.Interval())
+	assert.Equal(t, "/etc/your/moms/house", inst.configFilePath)
+	assert.Equal(t, "littlepiggies", inst.queryPath)
+	assert.Equal(t, 12, inst.timeout)
+}
+
+func TestErrorQueryStatsEndpointResponse(t *testing.T) {
+	//4xx HTTP status code test
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+	}))
+	endpoint := ts.URL + "/status/uwsgi"
+	ts.Close()
+
+	_, _, queryEndpointError := queryEndpoint(endpoint, 10)
+	assert.NotNil(t, queryEndpointError)
+
+	//Socket closed test
+	tsClosed := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	}))
+	tsClosed.Close()
+	closedEndpoint := tsClosed.URL + "/status/uwsgi"
+	_, queryClosedEndpointResponse, queryClosedEndpointError := queryEndpoint(closedEndpoint, 10)
+	assert.NotNil(t, queryClosedEndpointError)
+	assert.Equal(t, "", queryClosedEndpointResponse)
+}
+
+func convertURL(url string) (string, string) {
+	parts := strings.Split(url, ":")
+	ip := strings.Replace(parts[1], "/", "", -1)
+	port := parts[2]
+	return ip, port
+}
+
+func TestNerveUWSGIStatsCollect(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, rsp *http.Request) {
+		fmt.Fprint(w, getTestUWSGIStatsResponse())
+	}))
+	defer server.Close()
+
+	// assume format is http://ipaddr:port
+	ip, port := convertURL(server.URL)
+	minimalNerveConfig := util.CreateMinimalNerveConfig(map[string]util.EndPoint{
+		"test_service.things.and.stuff": util.EndPoint{ip, port},
+	})
+
+	tmpFile, err := ioutil.TempFile("", "fullerite_testing")
+	assert.Nil(t, err)
+	defer os.Remove(tmpFile.Name())
+
+	marshalled, err := json.Marshal(minimalNerveConfig)
+	assert.Nil(t, err)
+
+	_, err = tmpFile.Write(marshalled)
+	assert.Nil(t, err)
+
+	cfg := map[string]interface{}{
+		"configFilePath": tmpFile.Name(),
+		"queryPath":      "",
+	}
+
+	inst := getTestNerveUWSGIStats()
+	inst.Configure(cfg)
+
+	go inst.Collect()
+
+	actual := []metric.Metric{}
+	for i := 0; i < 6; i++ {
+		actual = append(actual, <-inst.Channel())
+	}
+
+	validateUWSGIStatsResults(t, actual)
+	validateStatsDimensions(t, actual, "test_service", port)
+	validateStatsEmptyChannel(t, inst.Channel())
+}
+
+func TestNonConflictingStatsServiceQueries(t *testing.T) {
+	goodServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, rsp *http.Request) {
+		fmt.Fprint(w, getTestUWSGIStatsResponse())
+	}))
+	defer goodServer.Close()
+
+	badServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, rsp *http.Request) {
+		return // no response
+	}))
+	defer badServer.Close()
+
+	goodIP, goodPort := convertURL(goodServer.URL)
+	badIP, badPort := convertURL(badServer.URL)
+	minimalNerveConfig := util.CreateMinimalNerveConfig(map[string]util.EndPoint{
+		"test_service.things.and.stuff":    util.EndPoint{goodIP, goodPort},
+		"other_service.does.lots.of.stuff": util.EndPoint{badIP, badPort},
+	})
+
+	tmpFile, err := ioutil.TempFile("", "fullerite_testing")
+	defer os.Remove(tmpFile.Name())
+	assert.Nil(t, err)
+
+	marshalled, err := json.Marshal(minimalNerveConfig)
+	assert.Nil(t, err)
+
+	_, err = tmpFile.Write(marshalled)
+	assert.Nil(t, err)
+
+	cfg := map[string]interface{}{
+		"configFilePath": tmpFile.Name(),
+		"queryPath":      "",
+	}
+
+	inst := getTestNerveUWSGIStats()
+	inst.Configure(cfg)
+
+	go inst.Collect()
+
+	actual := []metric.Metric{}
+	for i := 0; i < 6; i++ {
+		actual = append(actual, <-inst.Channel())
+	}
+
+	validateUWSGIStatsResults(t, actual)
+	validateStatsDimensions(t, actual, "test_service", goodPort)
+	validateStatsEmptyChannel(t, inst.Channel())
+}
