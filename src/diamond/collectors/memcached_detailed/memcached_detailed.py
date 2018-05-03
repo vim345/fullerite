59948f0e2a0671efa4eafc2a8889a046c8e98a40
Wendy Vivar <wendyv@yelp.com>
2018-05-03 15:51:25 -0700

    Added MemcachedDetailed collector and tests, fixed MemcacheCollector test, added default index server to tox

---
+++ src/diamond/collectors/memcached_detailed/memcached_detailed.py
@@ -0,0 +1,131 @@
+# coding=utf-8
+
+"""
+Collect memcached detailed stats
+
+
+#### Dependencies
+
+ * subprocess
+
+#### Example Configuration
+
+MemcachedDetailedCollector.conf
+
+```
+    enabled = True
+    hosts = localhost:11211, app-1@localhost:11212, app-2@localhost:11213, etc
+```
+
+TO use a unix socket, set a host string like this
+
+```
+    hosts = /path/to/blah.sock, app-1@/path/to/bleh.sock,
+```
+"""
+
+import diamond.collector
+import socket
+import re
+
+from collections import namedtuple
+
+PREFIX_PATTERN = 'PREFIX (?P<cache_name>[\w\-\.]+)'
+STATS_PATTERN = ' get (?P<get>[0-9]+) hit (?P<hit>[0-9]+) set (?P<set>[0-9]+) del (?P<del>[0-9]+)$'
+
+HOST_REGEX = re.compile('((?P<alias>.+)\@)?(?P<hostname>[^:]+)(:(?P<port>\d+))?')
+LINE_REGEX = re.compile(PREFIX_PATTERN + STATS_PATTERN)
+
+
+class MemcachedDetailedCollector(diamond.collector.Collector):
+
+    def get_default_config_help(self):
+        config_help = super(MemcachedDetailedCollector, self).get_default_config_help()
+        config_help.update({
+            'hosts': "List of hosts, and ports to collect. Set an alias by "
+            + " prefixing the host:port with alias@",
+        })
+        return config_help
+
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(MemcachedDetailedCollector, self).get_default_config()
+        config.update({
+            'path': 'memcached_detailed',
+            # Connection settings
+            'hosts': ['localhost:16666']
+        })
+        return config
+
+    def get_raw_stats(self, host, port):
+        data = ''
+        # connect
+        try:
+            if port is None:
+                sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                sock.connect(host)
+            else:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect((host, int(port)))
+            # request stats
+            sock.send('stats detail dump\n')
+            # something big enough to get whatever is sent back
+            data = sock.recv(4096)
+        except socket.error:
+            self.log.error('Failed to get detailed stats from %s:%s',
+                               host, port)
+        return data
+
+    def get_stats(self, host, port):
+        stats = set()
+        Stat = namedtuple('Stat', [
+                            'cache_name',
+                            'detailed_get',
+                            'detailed_hit',
+                            'detailed_set',
+                            'detailed_del'])
+
+        data = self.get_raw_stats(host, port)
+
+        for line in data.splitlines():
+            match = LINE_REGEX.match(line)
+            if match:
+                stats.add(Stat(
+                    cache_name=match.group('cache_name'),
+                    detailed_get=int(match.group('get')),
+                    detailed_hit=int(match.group('hit')),
+                    detailed_set=int(match.group('set')),
+                    detailed_del=int(match.group('del')),
+                ))
+        return stats
+
+    def collect(self):
+        stat_fields = set(['detailed_get', 'detailed_hit', 'detailed_set', 'detailed_del'])
+
+        hosts = self.config.get('hosts')
+
+        # Convert a string config value to be an array
+        if isinstance(hosts, basestring):
+            hosts = [hosts]
+
+        for host in hosts:
+            match = HOST_REGEX.match(host)
+            alias = match.group('alias')
+            hostname = match.group('hostname')
+            port = match.group('port')
+
+            if alias is None:
+                alias = hostname
+
+            stats = self.get_stats(hostname, port)
+
+            for stat in stats:
+                for field in stat_fields:
+                    self.dimensions = {
+                        'memcache_host': alias,
+                        'cache_name': stat.cache_name,
+                    }
+                    metric_name = '.'.join(['memcache', field])
+                    self.publish_cumulative_counter(metric_name, getattr(stat, field))
---
+++ src/diamond/collectors/memcached_detailed/test/testmemcached_detailed.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# coding=utf-8
+################################################################################
+
+from test import CollectorTestCase
+from test import get_collector_config
+from test import unittest
+from mock import Mock
+from mock import patch
+
+from diamond.collector import Collector
+from memcached_detailed import MemcachedDetailedCollector
+
+################################################################################
+
+
+class TestMemcachedDetailedCollector(CollectorTestCase):
+    def setUp(self):
+        config = get_collector_config('MemcachedDetailedCollector', {
+            'interval': 10,
+            'hosts': ['localhost:11211'],
+        })
+
+        self.collector = MemcachedDetailedCollector(config, None)
+
+    def assertPublishedManyMultiple(self, mock, expected_dict, expected_count=1):
+        for key, values in expected_dict.iteritems():
+            self.assertPublishedMultiple(mock, key, values, expected_count)
+
+        mock.reset_mock()
+
+    def assertPublishedMultiple(self, mock, key, values, expected_count):
+        calls = filter(lambda x: x[0][0] == key, mock.call_args_list)
+
+        actual_count = len(calls)
+        message = '%s: actual number of calls %d, expected %d' % (key, actual_count, expected_count)
+        self.assertEqual(actual_count, expected_count, message)
+
+        actual_values = [call[0][1] for call in calls]
+
+        for value in values:
+            message = '%d not a value for key %s' % (value, key)
+            self.assertIn(value, actual_values, message)
+
+    def test_import(self):
+        self.assertTrue(MemcachedDetailedCollector)
+
+    @patch.object(Collector, 'publish')
+    def test_real_stats(self, publish_mock):
+        patch_raw_stats = patch.object(
+            MemcachedDetailedCollector,
+            'get_raw_stats',
+            Mock(return_value=self.getFixture(
+                'stats_simple').getvalue()))
+
+        patch_raw_stats.start()
+        self.collector.collect()
+        patch_raw_stats.stop()
+
+
+        metrics = {
+            'memcache.detailed_get': [10, 192, 12, 100],
+            'memcache.detailed_set': [14, 1, 86],
+            'memcache.detailed_hit': [0, 150],
+            'memcache.detailed_del': [5, 9, 1],
+        }
+
+        self.assertPublishedManyMultiple(publish_mock, metrics, 7)
+
+################################################################################
+if __name__ == "__main__":
+    unittest.main()
