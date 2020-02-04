a69aba09bf20af1839094367840c3c3ca1744e23
Jayati Tiwari <jtiwari@yelp.com>
2020-02-04 09:25:12 -0800

    Bumping fullerite version to 0.6.66

--- src/fullerite/beatit/main.go
+++ src/fullerite/beatit/main.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	name    = "beatit"
-	version = "0.6.65"
+	version = "0.6.66"
 	desc    = "Stress test fullerite handlers"
 )
 
--- src/fullerite/main.go
+++ src/fullerite/main.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	name    = "fullerite"
-	version = "0.6.65"
+	version = "0.6.66"
 	desc    = "Diamond compatible metrics collector"
 )
 
