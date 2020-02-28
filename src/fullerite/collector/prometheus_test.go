7fbcda3e14ceb76fd01b58cfe26766da7be1c52c
Mohammad Mohtasham <mohm@yelp.com>
2020-02-28 15:09:10 -0800

    Added timeout to gRPC call and separate http and grpc getters from each other.

--- src/fullerite/collector/prometheus_test.go
+++ src/fullerite/collector/prometheus_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"fullerite/metric"
+	"fullerite/util"
 )
 
 func TestPrometheusNewPrometheus(t *testing.T) {
@@ -22,7 +23,7 @@ func TestPrometheusNewPrometheus(t *testing.T) {
 	assert.Equal(t, p.name, "Prometheus")
 }
 
-func TestPrometheusConfigure(t *testing.T) {
+func TestHTTPPrometheusConfigure(t *testing.T) {
 	expectedChan := make(chan metric.Metric)
 	var expectedLogger = defaultLog.WithFields(l.Fields{"collector": "fullerite"})
 
@@ -53,5 +54,42 @@ func TestPrometheusConfigure(t *testing.T) {
 	assert.Equal(t, endpoint.metricsWhitelist, map[string]bool{"123": true, "456": true})
 	assert.Equal(t, endpoint.metricsBlacklist, map[string]bool{"78": true})
 	assert.Equal(t, endpoint.generatedDimensions, map[string]string{"foo": "bar"})
-	assert.False(t, endpoint.isGrpc)
+	assert.Empty(t, endpoint.grpcGetter)
+	assert.Implements(t, (*util.HTTPGetter)(nil), endpoint.httpGetter)
+}
+
+func TestGRPCPrometheusConfigure(t *testing.T) {
+	expectedChan := make(chan metric.Metric)
+	var expectedLogger = defaultLog.WithFields(l.Fields{"collector": "fullerite"})
+
+	p := newPrometheus(expectedChan, 10, expectedLogger).(*Prometheus)
+
+	testConfig := []byte(`
+	{
+		"endpoints": [
+			{
+				"prefix":         "123/",
+				"url":            "https://etcd1.nowhere.com:2379/metrics",
+				"metrics_whitelist": ["123", "456"],
+				"metrics_blacklist": ["78"],
+				"generated_dimensions": {
+					"foo": "bar"
+				},
+				"isGrpc": true
+			}
+		]
+	}`)
+	testConfigMap := make(map[string]interface{})
+	json.Unmarshal(testConfig, &testConfigMap)
+	p.Configure(testConfigMap)
+
+	var endpoint *Endpoint = p.endpoints[0]
+
+	assert.Equal(t, endpoint.prefix, "123/")
+	assert.Equal(t, endpoint.metricsWhitelist, map[string]bool{"123": true, "456": true})
+	assert.Equal(t, endpoint.metricsBlacklist, map[string]bool{"78": true})
+	assert.Equal(t, endpoint.generatedDimensions, map[string]string{"foo": "bar"})
+	assert.Empty(t, endpoint.httpGetter)
+	assert.Empty(t, endpoint.headers)
+	assert.Implements(t, (*util.GRPCGetter)(nil), endpoint.grpcGetter)
 }
